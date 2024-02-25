9c10765dacd2c6ad9e61d8aa0b38738e3d988bf4
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-02-25 22:42:50 +0300

    add files

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var name: String = "Joseph Kamau"
+    println(name)
+    var age: Byte= 23
+    println(age)
+    var contacts: String="0701815843"
+    println(contacts)
+    var weight: Byte=65
+    println(weight)
+    val Kenyan: Boolean=true
+    println(Kenyan)
+
+}
